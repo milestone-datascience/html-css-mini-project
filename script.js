267958e57f6ac272b2248e752f47e5d96d87c76a
milestone-datascience <datascience.milestone@gmail.com>
2025-07-17 15:58:24 +0530

    New Project File Added

---
+++ script.js
@@ -0,0 +1,42 @@
+// let sumit = {
+//     name:"Sumit",
+//     age:23,
+//     isMarried:false,
+//     greet: function () {
+//         console.log("Hello");
+//         return "Hello"
+//     }
+// }
+
+// sumit['email'] = "sumit@gmail.com"
+
+// delete sumit['isMarried']
+
+
+// console.log(sumit["name"])
+// console.log(typeof sumit);
+
+// console.log(sumit);
+
+// console.log(typeof document);
+console.log(document.body.innerText);
+
+// document.title = "This changed by javascript"
+// document.querySelector("h1").innerText = "this heading was changed by javascript"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
